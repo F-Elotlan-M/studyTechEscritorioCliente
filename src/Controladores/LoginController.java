69d7209b5c008736b94dc57672b75728a3a4e08f
Elotlan <zS20020852@estudiantes.uv.mx>
2023-10-03 20:30:56 -0600

    primer commit

---
+++ src/Controladores/LoginController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Elotlan
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private TextField tfUsuario;
+    @FXML
+    private PasswordField pfContrasena;
+    @FXML
+    private Button btnCancelar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
